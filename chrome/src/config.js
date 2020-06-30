3b624875f5fbbaf06ccced75bb3313838bda0f08
andypols <andy@pols.co.uk>
2020-06-30 17:08:30 +0100

    More sensible default

--- chrome/src/config.js
+++ chrome/src/config.js
@@ -1,6 +1,6 @@
 export default {
   // What browserName should this data be associated with in the Kibana Dashboard?
-  browserName: "Andy's Browser",
+  browserName: "generic-browser",
 
   // What is the elastic endpoint for the index used by Kibana?
   // If you change this, you need to ensure this hostname appears in the manifest.json permissions
