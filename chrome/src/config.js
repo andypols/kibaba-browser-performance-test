e975122b3549e0a2628b97cf14a44c0b6b16ac85
andypols <andy@pols.co.uk>
2020-07-02 11:14:10 +0100

    Remove config

--- chrome/src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  // What browserName should this data be associated with in the Kibana Dashboard?
-  browserName: "generic-browser",
-
-  // What is the elastic endpoint for the index used by Kibana?
-  // If you change this, you need to ensure this hostname appears in the manifest.json permissions
-  elasticIndexUrl: 'http://localhost:9200',
-}
