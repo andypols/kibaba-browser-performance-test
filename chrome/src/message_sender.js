e5b94730745e30a78f852331f52f14d14dd3dfa7
andypols <andy@pols.co.uk>
2020-07-02 11:11:28 +0100

    Warn if elastic url not set up

--- chrome/src/message_sender.js
+++ chrome/src/message_sender.js
@@ -1,21 +1,27 @@
 import {getElasticIndexUrl} from './get-settings';
+import {isEmpty} from "lodash";
 
 const MESSAGE_BUFFER = 40;
 
 async function post(data) {
   console.log(`Posting...`)
   const elasticIndexUrl = await getElasticIndexUrl();
-  return fetch(`${elasticIndexUrl}/_bulk`, {
-    method: 'POST',
-    body: data,
-    mode: 'cors',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }).catch((err => {
-    console.log("Failed to send data:", err);
-  }));
+
+  if(isEmpty(elasticIndexUrl)) {
+    console.log('The elasticIndexUrl has not specified. Ignoring data post.')
+  } else {
+    fetch(`${elasticIndexUrl}/_bulk`, {
+      method: 'POST',
+      body: data,
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).catch((err => {
+      console.log("Failed to send data:", err);
+    }));
+  }
 }
 
 
