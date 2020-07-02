d4cf3157daa2cb0fc26528c7756c132ae7cda6e3
andypols <andy@pols.co.uk>
2020-07-02 18:45:13 +0100

    merge all monitor code to start cleaning up structure

--- chrome/src/monitor.js
+++ chrome/src/monitor.js
@@ -1,12 +1,51 @@
 import React from "react";
 import {render} from 'react-dom';
 
-import {getSystemInfo} from './utils';
 import MessageSender from './message_sender';
 import MonitorPage from './monitor-page.js'
 import {getBrowserName} from './get-settings';
 
 const messageSender = new MessageSender();
+
+function getCpuUsage(processors, processorsOld) {
+  const usage = []
+  for(let i = 0; i < processors.length; i++) {
+    const processor = processors[i]
+
+    if(processor.total === 0) continue
+
+    const processorOld = processorsOld[i]
+    usage.push(
+      processorOld
+        ? {
+          user: processor.user - processorOld.user,
+          kernel: processor.kernel - processorOld.kernel,
+          idle: processor.idle - processorOld.idle,
+          total: processor.total - processorOld.total,
+        }
+        : processor,
+    )
+  }
+  return usage
+}
+
+export async function getSystemInfo(cb, processorsOld = []) {
+  const cpu = await new Promise(resolve => {
+    chrome.system['cpu'].getInfo(resolve)
+  });
+
+  const data = {browser: await getBrowserName()}
+  let processors
+  if(cpu) {
+    processors = cpu.processors.map(({usage}) => usage)
+    data.cpu = {
+      usage: getCpuUsage(processors, processorsOld)
+    }
+  }
+
+  cb(data)
+  setTimeout(() => getSystemInfo(cb, processors), 1000);
+}
 
 getSystemInfo(({cpu: {usage}, browser}) => {
   const totals = usage.reduce((acc, core) => {
