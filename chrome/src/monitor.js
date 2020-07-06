a6103aaae1a2007032e8ce4532b184f04e914bb0
andypols <andy@pols.co.uk>
2020-07-06 13:39:35 +0100

    Move timer data to timed-data-collector.js

--- chrome/src/monitor.js
+++ chrome/src/monitor.js
@@ -3,9 +3,6 @@ import {render} from 'react-dom';
 
 import MessageSender from './message_sender';
 import MonitorPage from './monitor-page.js'
-import {getBrowserName} from './get-settings';
-import HeapStats from './stats/heap-stats';
-import SystemCpuStats from './stats/system-cpu-stats';
 import {EventDataCollector} from './event-data-collector';
 import {TimedDataCollector} from './timed-data-collector';
 
@@ -18,65 +15,7 @@ window.addEventListener("load", function() {
   timedDataCollector.monitor();
 });
 
-//
-// Data handlers for timer based things we collect every second
-//
 
-const timerHandlers = {
-  heap: new HeapStats(),
-  cpu: new SystemCpuStats()
-}
-
-function getCpuUsage(processors, processorsOld) {
-  const usage = []
-  for(let i = 0; i < processors.length; i++) {
-    const processor = processors[i]
-
-    if(processor.total === 0) continue
-
-    const processorOld = processorsOld[i]
-    usage.push(
-      processorOld
-        ? {
-          user: processor.user - processorOld.user,
-          kernel: processor.kernel - processorOld.kernel,
-          idle: processor.idle - processorOld.idle,
-          total: processor.total - processorOld.total,
-        }
-        : processor,
-    )
-  }
-  return usage
-}
-
-async function sendTimerData(cb, processorsOld = []) {
-  const cpu = await new Promise(resolve => {
-    chrome.system['cpu'].getInfo(resolve)
-  });
-
-  const processors = cpu.processors.map(({usage}) => usage)
-
-  const data = {
-    browser: await getBrowserName(),
-    cpu: getCpuUsage(processors, processorsOld)
-  }
-
-  cb(data)
-  setTimeout(() => sendTimerData(cb, processors), 1000);
-}
-
-sendTimerData(({cpu, browser}) => {
-  const browserData = {
-    '@timestamp': new Date().toISOString(),
-    browser: browser,
-    cpu: timerHandlers['cpu'].collect(cpu),
-    heap: timerHandlers['heap'].collect()
-  };
-
-  messageSender.postMessage('browser-cpu', browserData);
-});
-
-
-const root = document.createElement('div')
-document.body.appendChild(root)
-render(<MonitorPage/>, root)
+const root = document.createElement('div');
+document.body.appendChild(root);
+render(<MonitorPage/>, root);
--- chrome/src/timed-data-collector.js
+++ chrome/src/timed-data-collector.js
@@ -1,10 +1,65 @@
-import WebSocketStats from './stats/web-socket-stats';
-import {values, uniq} from 'lodash';
+import HeapStats from './stats/heap-stats';
+import SystemCpuStats from './stats/system-cpu-stats';
+import {getBrowserName} from './get-settings';
+
+function getCpuUsage(processors, processorsOld) {
+  const usage = []
+  for(let i = 0; i < processors.length; i++) {
+    const processor = processors[i]
+
+    if(processor.total === 0) continue
+
+    const processorOld = processorsOld[i]
+    usage.push(
+      processorOld
+        ? {
+          user: processor.user - processorOld.user,
+          kernel: processor.kernel - processorOld.kernel,
+          idle: processor.idle - processorOld.idle,
+          total: processor.total - processorOld.total,
+        }
+        : processor,
+    )
+  }
+  return usage
+}
+
+async function sendTimerData(cb, processorsOld = []) {
+  const cpu = await new Promise(resolve => {
+    chrome.system['cpu'].getInfo(resolve)
+  });
+
+  const processors = cpu.processors.map(({usage}) => usage)
+
+  const data = {
+    browser: await getBrowserName(),
+    cpu: getCpuUsage(processors, processorsOld)
+  }
+
+  cb(data)
+  setTimeout(() => sendTimerData(cb, processors), 1000);
+}
 
 export class TimedDataCollector {
   constructor(messageSender) {
+    this.messageSender = messageSender;
+    this.timerHandlers = {
+      heap: new HeapStats(),
+      cpu: new SystemCpuStats()
+    }
   }
 
   monitor() {
+    sendTimerData(({cpu, browser}) => {
+      const browserData = {
+        '@timestamp': new Date().toISOString(),
+        browser: browser,
+        cpu: this.timerHandlers['cpu'].collect(cpu),
+        heap: this.timerHandlers['heap'].collect()
+      };
+
+      this.messageSender.postMessage('browser-cpu', browserData);
+    });
+
   }
 }
