fd69b64bcb10b6251f34302d52e39c5f90c4ddde
andypols <andy@pols.co.uk>
2020-07-06 12:23:25 +0100

    Start extracting timer based data collection

--- chrome/src/monitor.js
+++ chrome/src/monitor.js
@@ -7,12 +7,15 @@ import {getBrowserName} from './get-settings';
 import HeapStats from './stats/heap-stats';
 import SystemCpuStats from './stats/system-cpu-stats';
 import {EventDataCollector} from './event-data-collector';
+import {TimedDataCollector} from './timed-data-collector';
 
 const messageSender = new MessageSender();
 const eventDataCollector = new EventDataCollector(messageSender);
+const timedDataCollector = new TimedDataCollector(messageSender);
 
 window.addEventListener("load", function() {
   eventDataCollector.monitor();
+  timedDataCollector.monitor();
 });
 
 //
---
+++ chrome/src/timed-data-collector.js
@@ -0,0 +1,10 @@
+import WebSocketStats from './stats/web-socket-stats';
+import {values, uniq} from 'lodash';
+
+export class TimedDataCollector {
+  constructor(messageSender) {
+  }
+
+  monitor() {
+  }
+}
